d37197630fdbe5ef52b7f3bc63feb315537b384f
BoeseKnight <ilia.lamashka@gmail.com>
2022-03-27 16:50:32 +0300

    repository creation

---
+++ src/main/java/by/epam/lamashka/Main.java
@@ -0,0 +1,12 @@
+package by.epam.lamashka;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private final static Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Shit");
+    }
+}
